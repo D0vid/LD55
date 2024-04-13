526dfe674da5831ea313c6f50c2718dfbd679476
David <david.de.schrijver@gmail.com>
2024-04-13 21:37:59 +0200

    Add scroll and bg

--- ui/view_manager.gd
+++ ui/view_manager.gd
@@ -1,18 +1,18 @@
 extends Control
 
 @onready var main_menu = $MainMenu
-@onready var book_viewport = $BookViewport
+@onready var game_viewport = $GameViewport
 @onready var transition_rect = $TransitionRect
 
 var views = []
 
 func _ready():
 	main_menu.get_node("%StartGameButton").connect("pressed", _on_start_game_button_pressed)
-	views = [main_menu, book_viewport]
+	views = [main_menu, game_viewport]
 	pass
 	
 func _on_start_game_button_pressed():
-	transition_to(book_viewport)
+	transition_to(game_viewport)
 	
 func transition_to(new_view):
 	var tween = create_tween()
